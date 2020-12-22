0b4f877b9fdba087c64982aa3520de1f6adde856
SaW2017 <s1wachter@edu.aau.at>
2020-12-22 13:45:13 +0100

    Add Input and Output basic Components

---
+++ src/components/Input.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="input">
+    <input type="text" maxlength="3" name="Temperature in Celsius" v-model="temp">
+    <p> Temperature: test {{temp}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "input",
+  data(){
+      return {
+        temp: ''
+      }
+  }
+}
+</script>
