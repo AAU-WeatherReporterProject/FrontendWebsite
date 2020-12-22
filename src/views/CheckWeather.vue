4e7a5a177a73ba4ddad5ec948648733ca85213da
SaW2017 <s1wachter@edu.aau.at>
2020-12-22 13:31:43 +0100

    Add Views Check Weather & Input Weather

---
+++ src/views/CheckWeather.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="checkWeather">
+    <h1>Check Weather</h1>
+    <Output></Output>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Output from "@/components/Output.vue";
+
+export default {
+  name: 'checkWeather',
+  components: {
+   Output
+  },
+  methods: {
+
+    }
+}
+
+</script>
---
+++ src/views/InputWeather.vue
@@ -0,0 +1,15 @@
+<template>
+ <div class="inputWeather">
+   <h1>Input your Weather-Data</h1>
+ </div>
+</template>
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: "InputWeather"-->
+<!--}-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
