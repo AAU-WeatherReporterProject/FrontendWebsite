67b65ada0c28a21f489b787338242ccd8d7303ef
SaW2017 <s1wachter@edu.aau.at>
2020-12-22 13:33:25 +0100

    Add Instruction Component with Welcome & Instruction-Text

---
+++ src/components/Instruction.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <p>In this App you have the possibility to check detailed weather around you.
+      It`s even better - You can help make this information even more detailed by adding
+      your weather from your location.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Instruction',
+  components: {},
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
