a87348f7473331bc4d51a1d59e9329d0e43d505a
SaW2017 <s1wachter@edu.aau.at>
2020-12-28 13:46:48 +0100

    Adapt Check Test Url

--- tests/e2e/custom-commands/openCheckWeatherCheckUrlValid.js
+++ tests/e2e/custom-commands/openCheckWeatherCheckUrlValid.js
@@ -1,6 +1,6 @@
 module.exports = {
   command: async function () {
     this.url(this.launch_url + 'checkWeather')
-    this.assert.equal(this.launch_url + 'checkWeather', 'http://localhost:8081/checkWeather')
+    this.assert.equal(this.launch_url + 'checkWeather', 'http://localhost:8080/checkWeather')
   }
 }
