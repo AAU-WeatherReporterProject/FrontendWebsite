0c8b6730c47dcd4a8248709caf0ec832dbfd40ea
SaW2017 <s1wachter@edu.aau.at>
2020-12-28 10:56:22 +0100

    Refactor Test opent the weather page in custom method

--- tests/e2e/specs/test-Check-Weather-Output.js
+++ tests/e2e/specs/test-Check-Weather-Output.js
@@ -4,18 +4,16 @@
 module.exports = {
   'Test Link to Check Weather/Output and see if output div is present': browser => {
     browser
-      .init()
-      .waitForElementVisible('#app')
+        .init()
+        .waitForElementVisible('#app')
         .click('#check-w-link')
-      .assert.elementPresent('#output')
+        .assert.elementPresent('#output')
       .end()
   },
 
   'Test if Search-box is present': browser => {
     browser
-        .init()
-        .waitForElementVisible('#app')
-        .click('#check-w-link')
+        .url(browser.launch_url + "checkWeather")
         .waitForElementVisible('#output')
         .assert.elementPresent('.search-box')
         .end()
@@ -23,28 +21,36 @@ module.exports = {
 
   'Test if Search-bar is present': browser => {
     browser
-        .init()
-        .waitForElementVisible('#app')
-        .click('#check-w-link')
+        .openCheckWeatherPage()
         .assert.elementPresent('.search-bar')
         .end()
   },
 
   'Test if Weather-Output is present': browser => {
     browser
-        .init()
-        .waitForElementVisible('#app')
-        .click('#check-w-link')
+        .openCheckWeatherPage()
         .assert.elementPresent('.weather-output')
         .end()
   },
 
   'Test if location-box is present': browser => {
     browser
-        .init()
-        .waitForElementVisible('#app')
-        .click('#check-w-link')
+        .openCheckWeatherPage()
         .assert.elementPresent('.location-box')
         .end()
   },
+
+  'Test WEATHER-Output with openCheckWeather': browser => {
+    browser
+        .openCheckWeatherCheckUrlValid()
+        .assert.elementPresent('#output')
+        .end()
+  },
+
+  'Test WEATHER-Output contains all Divs': browser => {
+    browser
+        .checkWeatherOutputContainsAllDivs()
+        .assert.elementPresent('.sky-state')
+        .end()
+  }
 }
