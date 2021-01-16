3774a5ac22f6dc90720266d4681d8d32fd9b9d50
SaW2017 <s1wachter@edu.aau.at>
2021-01-16 12:17:24 +0100

    Send Input to API

--- src/store/index.js
+++ src/store/index.js
@@ -5,23 +5,90 @@ import api from '@/services/backend-api';
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  data:{
+    inputMsg: 'Waiting'
   },
-  mutations: {
+  state: {
   },
   actions: {
     getMeasurements: async function () {
-      let weather = '';
+      let msg;
       try {
         const response = await api.getMeasurements();
-        weather = response;
+        msg = response;
       } catch (e) {
-        weather = 'Weather Data could not be fetched.';
+        msg = 'Weather Data could not be fetched.';
         console.error(e);
       }
-      return weather;
+      return msg;
+    },
+
+    async getWeatherData([key, from, to]){
+      let msg = '';
+      try{
+        const response = await api.getMeasurements();
+        // for(let measurement of response.data){
+        //   message += `\nname: ${measurement.name}; \nlocation: ${measurement.location}; \nkey: ${measurement.measurementKey}`;
+        // }
+        // weather = response;
+        msg = key;
+      }
+      catch(e){
+        msg = 'Weather Data could not be fetched.';
+        console.error(e);
+      }
+      return msg;
+    },
+
+    async sendWeatherData({commit}, weather){
+      commit('saveWeather', weather);
+      return this.inputMsg;
+    }
+  },
+
+  mutations:{
+    async saveWeather(state, weatherinfo) {
+     let key = weatherinfo.key;
+     let temperature = weatherinfo.temperature;
+     let skyState = weatherinfo.skyState;
+      try {
+        await api.sendWeatherData({
+          metadata: {key},
+          measurements: [{temperature, skyState}]
+        });
+        this.inputMsg = 'Data send';
+      } catch (e) {
+        this.inputMsg = 'Error while sending data';
+        console.error(e);
+      }
+      return this.inputMsg;
     }
   },
   modules: {
   }
 })
+// async sendWeatherData([key, temperature, skyState]){
+//   let weather = '';
+//   temperature = +temperature;//.replace(',', '.');
+//   skyState = +skyState;
+//   if(isNaN(temperature)){
+//     weather = 'Invalid temperature';
+//   }
+//   else if(isNaN(skyState) || skyState < 0 || skyState > 4){
+//     weather = 'Invalid sky state';
+//   }
+//   else{
+//     try{
+//       await api.sendWeatherData({
+//         metadata: {key},
+//         measurements: [{temperature, skyState}]
+//       });
+//       weather = 'Data send';
+//     }
+//     catch(e){
+//       weather = 'Error while sending data';
+//       console.error(e);
+//     }
+//   }
+//   return weather;
+// },
