74cf9b9a38ce652c87f65048ddca8f2a035f5263
SaW2017 <s1wachter@edu.aau.at>
2021-01-22 15:39:21 +0100

    Changed the View of Home Instruction

--- src/views/HomeInstruction.vue
+++ src/views/HomeInstruction.vue
@@ -18,3 +18,39 @@ export default {
   }
 }
 </script>
+
+<style>
+
+h3 {
+  margin: 40px 0 0;
+}
+
+.home{
+  padding: 10px;
+}
+
+p {
+  display: block;
+  width: 50%;
+  padding: 15px;
+  margin: auto;
+  margin-bottom: 10%;
+
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+a {
+  color: #42b983;
+}
+</style>
