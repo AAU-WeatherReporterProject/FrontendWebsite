efb9f72b88773d78fedbc41481c6afdb3f6b88b5
SaW2017 <s1wachter@edu.aau.at>
2021-01-23 15:05:30 +0100

    Refactore Weatherlist to get rid of bug

--- src/components/WeatherOverview.vue
+++ src/components/WeatherOverview.vue
@@ -2,7 +2,7 @@
   <div id="output">
     <div class="info">Please click on the Location you want further information for.</div>
 <!--    TODO Nachricht falls noch kein Wetter hinzugefüpgt wurde-->
-    <div v-for="w in weatherlist.data" v-bind:key="w.key" class="weather-output">
+    <div v-for="w in weatherlist" v-bind:key="w.key" class="weather-output">
       <div class="location-box">
         <div class="location big"><router-link v-bind:to="'/checkWeather/' + w.location" >{{ w.location }}</router-link></div>
       </div>
@@ -13,13 +13,13 @@
 export default {
   name: "WeatherOverview",
   data() {
-    return {weatherlist: undefined}
+    return {weatherlist: []}
   },
   created: async function () {
     try {
       let response;
       response = await this.$store.dispatch('getMeasurements');
-      this.weatherlist = response;
+      this.weatherlist = response.data;
     } catch (e) {
       console.log(e);
     }
