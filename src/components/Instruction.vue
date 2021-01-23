2e0424b6dc11b858ac111f32464049fcb52a2190
SaW2017 <s1wachter@edu.aau.at>
2021-01-23 16:14:08 +0100

    Changed to common info-class for reusability

--- src/components/Instruction.vue
+++ src/components/Instruction.vue
@@ -2,7 +2,7 @@
   <div class="instruction">
     <h1>{{ msg }}</h1>
 
-    <p id="instruction-text">In this App you have the possibility to check detailed weather around you.
+    <p class="info">In this App you have the possibility to check detailed weather around you.
       It`s even better - You can help make this information even more detailed by adding
       your weather from your location.</p>
   </div>
