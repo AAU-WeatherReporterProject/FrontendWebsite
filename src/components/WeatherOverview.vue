bfddf2cebdfa2458d47c918f329d7646fc6c7acd
SaW2017 <s1wachter@edu.aau.at>
2021-01-23 18:21:06 +0100

    Add Error Message if fetching Data didn't work

--- src/components/WeatherOverview.vue
+++ src/components/WeatherOverview.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="output">
     <div class="info">Please click on the Location you want further information for.</div>
-<!--    TODO Nachricht falls noch kein Wetter hinzugefüpgt wurde-->
     <div v-for="w in weatherlist" v-bind:key="w.key" class="weather-output">
       <div class="location-box">
         <div class="location big"><router-link v-bind:to="'/checkWeather/' + w.location" >{{ w.location }}</router-link></div>
@@ -17,13 +16,16 @@ export default {
   },
   created: async function () {
     try {
+      this.$forceUpdate();
       let response;
       response = await this.$store.dispatch('getMeasurements');
       this.weatherlist = response.data;
     } catch (e) {
       this.$bvToast.toast('Receiving of measurements failed', {
-        title: `Error`,
-        variant: 'error' //'error', 'warning', 'success'
+        title: 'Error!',
+        variant: 'danger',
+        toaster: 'b-toaster-top-center',
+        autoHideDelay: 5000
       });
     }
   }
