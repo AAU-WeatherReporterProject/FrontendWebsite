89c8a3b1978936ba817d907edd803c6a1cb6b6df
SaW2017 <s1wachter@edu.aau.at>
2021-02-04 14:01:36 +0100

    Make API on Public

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  
+  publicPath:{
+    proxy: {
+      '/api': {
+        target: 'http://35.157.65.50:8098', // this configuration needs to correspond to the Spring Boot backends' application.properties server.port
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
   // Change build paths to make them Maven compatible
   // see https://cli.vuejs.org/config/
   devServer: {
