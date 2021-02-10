35bf73fd245966b512bc79dfb400c6f8b0415723
SaW2017 <s1wachter@edu.aau.at>
2021-02-10 18:21:20 +0100

    REFACTORE and ADD Navigation Bar Tests

--- tests/e2e/specs/test-nav-bar.js
+++ tests/e2e/specs/test-nav-bar.js
@@ -1,8 +1,15 @@
 module.exports = {
     'Test if Nav-Bar contains 3 Elements': browser => {
         browser
-            .openHomepage()
+            .countNavigationElements()
             .assert.elementPresent('#nav')
+            .end()
+    },
+    'Test if Nav-Bar contains Home': browser => {
+        const page = browser.page.instructionPage();
+        page
+            .navigate()
+            .assert.containsText('@navBar', 'Home')
             .end()
     }
 }
