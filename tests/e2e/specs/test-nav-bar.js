5e44b5614f61785a72190d396b96e58a680ddb67
SaW2017 <s1wachter@edu.aau.at>
2021-02-11 20:19:28 +0100

    REFACTORE test Nav-Bar - delete obsolete Variable and ADD testing the Links

--- tests/e2e/specs/test-nav-bar.js
+++ tests/e2e/specs/test-nav-bar.js
@@ -5,11 +5,35 @@ module.exports = {
             .assert.elementPresent('#nav')
             .end()
     },
+
     'Test if Nav-Bar contains Home': browser => {
-        const page = browser.page.instructionPage();
-        page
+        browser.page.instructionPage()
             .navigate()
             .assert.containsText('@navBar', 'Home')
+            .end()
+    },
+
+    'Test if Nav-Bar contains Weather Overview': browser => {
+        browser.page.instructionPage()
+            .navigate()
+            .assert.containsText('@navBar', 'Weather Overview')
+            .end()
+    },
+
+    'Test if Nav-Bar contains Input Weather': browser => {
+       browser.page.instructionPage()
+            .navigate()
+            .assert.containsText('@navBar', 'Input Weather')
+            .end()
+    },
+
+    'Test if Nav-Bar-Link to Weather Overview works': browser => {
+        browser.page.instructionPage()
+            .navigate()
+            .click('@overviewLink');
+        browser.page.overviewWeatherPage()
+            .navigate()
+            .assert.containsText('@headline', 'Weather Overview')
             .end()
     }
 }
