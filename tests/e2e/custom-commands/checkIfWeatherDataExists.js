9f3eae2f3e4b7723ecced2b6c8c7450becebc07c
SaW2017 <s1wachter@edu.aau.at>
2021-02-11 21:18:40 +0100

    REFACTORE

--- tests/e2e/custom-commands/checkIfWeatherDataExists.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    command: async function () {
-        const result = await this.elements('css selector', '.weather-box')
-        this.assert.notEqual(result.value.length, 0, 'Check if Weather-Data is present: yes != 0')
-    }
-}
