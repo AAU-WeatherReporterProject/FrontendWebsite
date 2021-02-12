17e52b6177d153ee84d4c509de2cea14eb14974c
SaW2017 <s1wachter@edu.aau.at>
2021-02-12 07:40:25 +0100

    Test

--- tests/e2e/custom-commands/openOverviewWeatherCheckUrlValid.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  command: async function () {
-    this.url(this.launch_url + 'overviewWeather')
-    this.assert.equal(this.launch_url + 'overviewWeather', 'http://localhost:8080/overviewWeather')
-  }
-}
